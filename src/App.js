b3fd57147ed83bdbd955d02d66b887c586cffbd5
Henry-Cevallos <henry.cevallos95@myhunter.cuny.edu>
2021-04-08 19:43:41 -0400

    added zip implementation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,116 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
-
-function App() {
+function City({data}) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="card mb-4">
+      <div className="card-header">
+        { data.City}
+      </div>
+      <div className="card-body">
+        <ul>
+          <li>State: {data.State}</li>
+          <li>Location: ({data.Lat}, {data.Long})</li>
+        </ul>
+      </div>
     </div>
   );
 }
 
+function ZipSearchField({zipCode, onChange}) {
+  return (
+    <div>
+      <form className="form-inline my-4">
+        <label>Zip Code:</label>
+        <input
+          type="text"
+          className="form-control ml-2"
+          value={zipCode}
+          onChange={onChange}
+        />
+      </form>
+    </div>
+  );
+}
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cities: [],
+      zipCode: '',
+    }
+  }
+  zipChanged(event) {
+    let zip = event.target.value;
+
+    if(zip.length === 5){
+    fetch(`http://ctp-zip-api.herokuapp.com/zip/${zip}`)
+      .then((res) => res.json())
+      .then((body) => {
+        console.log(body);
+        this.setState({
+          cities: body
+        })
+      })
+      .catch((err) => {
+        console.log("err")
+        this.setState({ //if there is errors make cities null
+          cities: []
+        })
+      })
+    } else {
+      console.log("err")
+        this.setState({ //if there is errors make cities null
+          cities: []
+        })
+    }
+
+    this.setState({
+      zipCode: event.target.value
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h2>Zip Code Search</h2>
+        </div>
+        <div className="container">
+          <div className="row">
+            <div className="col">
+              <ZipSearchField
+                zipCode={this.state.zipCode}
+                onChange={(e) => this.zipChanged(e)}
+              />
+            </div>
+          </div>
+          {
+            this.state.cities.length === 0 
+              ? <p>No Results</p>
+              : this.state.cities.map((c, index) => {
+              return (
+                <div className="row" key ={index}>
+                  <div className="col">
+                    <City 
+                      data={c}
+                      
+                    />
+                  </div>
+                </div>
+              )
+            })
+          }
+        </div>
+      </div>
+    );
+  }
+}
 export default App;
+/*
+npm install -g npm@latest
+TODO:
+- Display more data about each city  -DONE
+- remove results when extra characters are typed -DONE
+- display "no results" if the zip is incorrect instead of empty -DONE
+- add checks to prevent multiple requests if we know zip is invalid format -DONE
+*/
